2577627e3f822f712f0736057f049422d974d33c
CesarAlexis <cesaralexisitz@gmail.com>
2022-06-23 22:31:25 -0500

    [UPDATE] Sobreescritura del método dispatch
    Sobreescritura del método dispatch y como sobrescribirlo en las vistas basadas en clases.

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -5,4 +5,5 @@ app_name = 'erp'
 
 urlpatterns = [
     path('category/list/', CategoryListView.as_view(), name='category_list'),
+    path('category/list2/', category_list, name='category_list2'),
 ]
--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 
 from core.erp.models import Category
@@ -15,6 +15,11 @@ def category_list(request):
 class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            return redirect('erp:category_list2')
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
