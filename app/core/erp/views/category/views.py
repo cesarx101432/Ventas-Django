5e375982d16beef5dde5d9e9d39efd6e3476ce00
CesarAlexis <cesaralexisitz@gmail.com>
2022-06-23 22:54:20 -0500

    [UPDATE] Sobreescritura del método post csrf_exempt
    Sobreescritura del método post csrf_exempt

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.views.generic import ListView
 from django.utils.decorators import method_decorator
 from core.erp.models import Category
@@ -17,9 +19,14 @@ class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
 
-    @method_decorator(login_required)
+    @method_decorator(csrf_exempt)
+    #@method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {'name': 'William'}
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
