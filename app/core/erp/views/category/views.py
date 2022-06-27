f407ac16daf5d95a9677b8de40374e1397d83c61
CesarAlexis <cesaralexisitz@gmail.com>
2022-06-26 20:39:47 -0500

    [UPDATE]  Implementando ajax para el eliminar un registro
     Implementando ajax para el eliminar un registro

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -118,6 +118,19 @@ class CategoryDeleteView(DeleteView):
     template_name = 'category/delete.html'
     success_url = reverse_lazy('erp:category_list')
 
+    #@method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            self.object.delete()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Eliminación de una Categoria'
