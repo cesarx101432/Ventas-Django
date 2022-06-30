7094fd852a59173bbe016ca366a0284f3e7f6a4a
CesarAlexis <cesaralexisitz@gmail.com>
2022-06-29 22:42:12 -0500

    [UPDATE] Validando nuestras vistas (login_required, next, LOGIN_URL)
    Validando nuestras vistas (login_required, next, LOGIN_URL)

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/erp/category/list/'
+LOGIN_REDIRECT_URL = '/erp/dashboard/'
 
 LOGOUT_REDIRECT_URL = '/login/'
+
+LOGIN_URL = '/login/'
