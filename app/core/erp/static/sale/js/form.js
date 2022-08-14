1b9048ab0ab3a5798a1cbea044a1526dd83b34aa
CesarAlexis <cesaralexisitz@gmail.com>
2022-08-14 00:17:19 -0500

    [ UPDATE ] Agregando items a mi detalle de la venta

--- app/core/erp/static/sale/js/form.js
+++ app/core/erp/static/sale/js/form.js
@@ -1,3 +1,71 @@
+var vents = {
+    items: {
+        cli: '',
+        date_joined: '',
+        subtotal: 0.00,
+        iva: 0.00,
+        total: 0.00,
+        products: []
+    },
+    add: function(item){
+        this.items.products.push(item);
+        this.list();
+    },
+    list: function () {
+        $('#tblProducts').DataTable({
+            responsive: true,
+            autoWidth: false,
+            destroy: true,
+            data: this.items.products,
+            columns: [
+                {"data": "id"},
+                {"data": "name"},
+                {"data": "cat.name"},
+                {"data": "pvp"},
+                {"data": "cant"},
+                {"data": "subtotal"},
+            ],
+            columnDefs: [
+                {
+                    targets: [0],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '<a rel="remove" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
+                    }
+                },
+                {
+                    targets: [-3],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '$' + parseFloat(data).toFixed(2);
+                    }
+                },
+                {
+                    targets: [-2],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '<input type="text" name="cant" class="form-control form-control-sm" autocomplete="off" value="'+row.cant+'">';
+                    }
+                },
+                {
+                    targets: [-1],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '$' + parseFloat(data).toFixed(2);
+                    }
+                },
+            ],
+            initComplete: function (settings, json) {
+
+            }
+        });
+    },
+};
+
 $(function () {
     $('.select2').select2({
         theme: "bootstrap4",
@@ -19,5 +87,38 @@ $(function () {
         boostat: 5,
         maxboostedstep: 10,
         postfix: '%'
+    });
+
+    // search products
+
+    $('input[name="search"]').autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: window.location.pathname,
+                type: 'POST',
+                data: {
+                    'action': 'search_products',
+                    'term': request.term
+                },
+                dataType: 'json',
+            }).done(function (data) {
+                response(data);
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                //alert(textStatus + ': ' + errorThrown);
+            }).always(function (data) {
+
+            });
+        },
+        delay: 500,
+        minLength: 1,
+        select: function (event, ui) {
+            event.preventDefault();
+            console.clear();
+            ui.item.cant = 1;
+            ui.item.subtotal = 0.00;
+            console.log(vents.items);
+            vents.add(ui.item);
+            $(this).val('');
+        }
     });
 });
