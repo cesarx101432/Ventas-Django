2955ba24a9c3cf86d62838656ebdde8ffdcccef6
CesarAlexis <cesaralexisitz@gmail.com>
2022-08-17 23:22:02 -0500

    [ UPDATE ] Generar reportes en Django
    https://www.daterangepicker.com/

--- app/core/reports/views.py
+++ app/core/reports/views.py
@@ -1,11 +1,45 @@
+from django.http import JsonResponse
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 
+from core.erp.models import Sale
 from core.reports.forms import ReportForm
 
 
 class ReportSaleView(TemplateView):
     template_name = 'sale/report.html'
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'search_report':
+                data = []
+                start_date = request.POST.get('start_date', '')
+                end_date = request.POST.get('end_date', '')
+                search = Sale.objects.all()
+                if len(start_date) and len(end_date):
+                    search = search.filter(date_joined__range=[start_date, end_date])
+                for s in search:
+                    data.append([
+                        s.id,
+                        s.cli.names,
+                        s.date_joined.strftime('%Y-%m-%d'),
+                        format(s.subtotal, '.2f'),
+                        format(s.iva, '.2f'),
+                        format(s.total, '.2f'),
+                    ])
+            else:
+                data['error'] = 'Ha ocurrido un error'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
