706aa162b1b3a2a6d3a0bea006c92f5d04407f8c
cesarx101432 <90880813+cesarx101432@users.noreply.github.com>
2023-01-21 16:18:55 -0600

    Gráficos en Django Parte II

--- app/core/erp/views/dashboard/views.py
+++ app/core/erp/views/dashboard/views.py
@@ -1,13 +1,44 @@
-from django.db.models import Sum
-from django.db.models.functions import Coalesce
-from django.views.generic import TemplateView
 from datetime import datetime
 
-from core.erp.models import Sale
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
+
+from core.erp.models import Sale, Product, DetSale
 
 
 class DashboardView(TemplateView):
     template_name = 'dashboard.html'
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'get_graph_sales_year_month':
+                data  = {
+                    'name': 'Porcentaje de venta',
+                    'showInLegend': False,
+                    'colorByPoint': True,
+                    'data': self.get_graph_sales_year_month()
+                }
+            elif action == 'get_graph_sales_products_year_month':
+                data = {
+                    'name': 'Porcentaje',
+                    'colorByPoint': True,
+                    'data': self.get_graph_sales_products_year_month(),
+                }
+            else:
+                data['error'] = 'Ha ocurrido un error'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
 
     def get_graph_sales_year_month(self):
         data = []
@@ -19,6 +50,30 @@ class DashboardView(TemplateView):
                 for s in search:
                     total += s.total
                 data.append(float(total))
+        except:
+            pass
+        return data
+
+    def get_graph_sales_products_year_month(self):
+        data = []
+        year = datetime.now().year
+        month = datetime.now().month
+        try:
+            for p in Product.objects.all():
+                
+                total = 0;
+                search = DetSale.objects.filter(sale__date_joined__year=year, sale__date_joined__month=month, prod_id=p.id)
+                for s in search:
+                    total =+ s.subtotal
+                    print(s.subtotal)
+
+
+              
+                if total > 0:
+                    data.append({
+                        'name': p.name,
+                        'y': float(total)
+                    })
         except:
             pass
         return data
