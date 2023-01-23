68b0e2c8a886a7ab84131a250792d3f3b0a643b7
cesarx101432 <90880813+cesarx101432@users.noreply.github.com>
2023-01-23 13:38:09 -0600

    Reseteo de contraseña de un usuario en Django Parte II

--- app/core/login/forms.py
+++ app/core/login/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from core.user.models import User
+
 
 class ResetPasswordForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
@@ -7,3 +9,15 @@ class ResetPasswordForm(forms.Form):
         'class': 'form-control',
         'autocomplete': 'off'
     }))
+
+    def clean(self):
+        cleaned = super().clean()
+        if not User.objects.filter(username=cleaned['username']).exists():
+            #self._errors['error'] = self._errors.get('error', self.error_class())
+            #self._errors['error'].append('El usuario no existe')
+            raise forms.ValidationError('El usuario no existe')
+        return cleaned
+
+    def get_user(self):
+        username = self.cleaned_data.get('username')
+        return User.objects.get(username=username)
--- app/core/login/views.py
+++ app/core/login/views.py
@@ -1,13 +1,22 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import FormView, RedirectView
 
 import app.settings as setting
+from app import settings
 from core.login.forms import ResetPasswordForm
+from core.user.models import User
 
 
 class LoginFormView(LoginView):
@@ -57,12 +66,49 @@ class ResetPasswordView(FormView):
     template_name = 'login/resetpwd.html'
     success_url = reverse_lazy(setting.LOGIN_REDIRECT_URL)
 
+    @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
-    def form_valid(self, form):
-        pass
-        return HttpResponseRedirect(self.success_url)
+    def send_email_reset_pwd(self, user):
+        data = {}
+        try:
+            mailServer = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
+            mailServer.starttls()
+            mailServer.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+
+            email_to = user.email
+            mensaje = MIMEMultipart()
+            mensaje['From'] = settings.EMAIL_HOST_USER
+            mensaje['To'] = email_to
+            mensaje['Subject'] = 'Reseteo de contraseña'
+
+            content = render_to_string('login/send_email.html', {
+                'user': user,
+                'link_resetpwd': '',
+                'link_home': ''
+            })
+            mensaje.attach(MIMEText(content, 'html'))
+
+            mailServer.sendmail(settings.EMAIL_HOST_USER,
+                                email_to,
+                                mensaje.as_string())
+        except Exception as e:
+            data['error'] = str(e)
+        return data
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            form = ResetPasswordForm(request.POST)  # self.get_form()
+            if form.is_valid():
+                user = form.get_user()
+                data = self.send_email_reset_pwd(user)
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
