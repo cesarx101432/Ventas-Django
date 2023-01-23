c72d16680b87c8520fb8887bb0eca88d2bd9e130
cesarx101432 <90880813+cesarx101432@users.noreply.github.com>
2023-01-22 20:45:42 -0600

    Revert "Perfiles dentro del dashboard del usuario Parte I"
    This reverts commit d95ac505b5c07a182d86008d6c25a3ea5818f752.

--- app/core/reports/views.py
+++ app/core/reports/views.py
@@ -21,6 +21,7 @@ class ReportSaleView(TemplateView):
     def post(self, request, *args, **kwargs):
         data = {}
         try:
+            subtotal = iva = total = 0
             action = request.POST['action']
             if action == 'search_report':
                 data = []
@@ -30,6 +31,9 @@ class ReportSaleView(TemplateView):
                 if len(start_date) and len(end_date):
                     search = search.filter(date_joined__range=[start_date, end_date])
                 for s in search:
+                    subtotal +=  s.subtotal
+                    iva +=  s.iva
+                    total +=  s.total
                     data.append([
                         s.id,
                         s.cli.names,
@@ -38,10 +42,6 @@ class ReportSaleView(TemplateView):
                         format(s.iva, '.2f'),
                         format(s.total, '.2f'),
                     ])
-
-                subtotal = search.aggregate(r=Coalesce(Sum('subtotal'), 0)).get('r')
-                iva = search.aggregate(r=Coalesce(Sum('iva'), 0)).get('r')
-                total = search.aggregate(r=Coalesce(Sum('total'), 0)).get('r')
 
                 data.append([
                     '---',
@@ -64,4 +64,3 @@ class ReportSaleView(TemplateView):
         context['list_url'] = reverse_lazy('sale_report')
         context['form'] = ReportForm()
         return context
-
